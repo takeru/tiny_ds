a14abe501edebde02d7c38de29373fd595717ed1
takeru <sasaki.takeru@gmail.com>
2010-02-25 17:56:42 +0900

    GAE_VER

--- examples/basic/set_classpath_for_jruby.sh
+++ examples/basic/set_classpath_for_jruby.sh
@@ -1,13 +1,14 @@
 # source set_classpath_for_jruby.sh
 
 PROJ_HOME=$(cd $(dirname $0);pwd)
-APPENGINE_SDK_GEM=/opt/local/lib/ruby/gems/1.8/gems/appengine-sdk-1.3.0
+GAE_VER=1.3.0
+APPENGINE_SDK_GEM=/opt/local/lib/ruby/gems/1.8/gems/appengine-sdk-$GAE_VER
 
 CLASSPATH=""
-CLASSPATH=$CLASSPATH:$PROJ_HOME/WEB-INF/lib/appengine-api-1.0-sdk-1.3.0.jar
-CLASSPATH=$CLASSPATH:$PROJ_HOME/WEB-INF/lib/appengine-api-labs-1.3.0.jar
-CLASSPATH=$CLASSPATH:$APPENGINE_SDK_GEM/appengine-java-sdk-1.3.0/lib/shared/appengine-local-runtime-shared.jar
-CLASSPATH=$CLASSPATH:$APPENGINE_SDK_GEM/appengine-java-sdk-1.3.0/lib/impl/appengine-api-stubs.jar
-CLASSPATH=$CLASSPATH:$APPENGINE_SDK_GEM/appengine-java-sdk-1.3.0/lib/impl/appengine-local-runtime.jar
+CLASSPATH=$CLASSPATH:$PROJ_HOME/WEB-INF/lib/appengine-api-1.0-sdk-$GAE_VER.jar
+CLASSPATH=$CLASSPATH:$PROJ_HOME/WEB-INF/lib/appengine-api-labs-$GAE_VER.jar
+CLASSPATH=$CLASSPATH:$APPENGINE_SDK_GEM/appengine-java-sdk-$GAE_VER/lib/shared/appengine-local-runtime-shared.jar
+CLASSPATH=$CLASSPATH:$APPENGINE_SDK_GEM/appengine-java-sdk-$GAE_VER/lib/impl/appengine-api-stubs.jar
+CLASSPATH=$CLASSPATH:$APPENGINE_SDK_GEM/appengine-java-sdk-$GAE_VER/lib/impl/appengine-local-runtime.jar
 echo CLASSPATH=$CLASSPATH
 export CLASSPATH
